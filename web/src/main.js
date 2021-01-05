71981232eea1e73fbf5e4ffd2c5c5a58098784bc
zhuxianglong <56494565@qq.com>
2021-01-05 18:30:05 +0800

    no message

--- web/src/main.js
+++ web/src/main.js
@@ -1,4 +1,7 @@
-import { createApp } from 'vue'
+import {createApp} from 'vue'
+import Antd from 'ant-design-vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+const app = createApp(App)
+app.use(Antd).mount('#app')
